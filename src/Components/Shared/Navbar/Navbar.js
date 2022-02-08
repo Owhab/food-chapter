d5e6dcdd78fa651527e9898db9f2bc01f07ce715
Abdul Owhab <owhab.cse@gmail.com>
2022-02-09 05:23:27 +0600

    Navbar Fixed

--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -10,10 +10,18 @@ const Navbar = () => {
         <img src={Logo} alt="" className="brand-logo" />
         <div>
           <ul className="nav-items">
-            <span className="px-5 py-2">Home</span>
-            <span className="px-5 py-2">About</span>
-            <span className="px-5 py-2">Contact</span>
-            <span className="login-btn px-5 py-2">Login</span>
+            <span className="lg:px-5 lg:py-2 md:px-3 md:py-3 sm:px-2 sm:py-2 ">
+              Home
+            </span>
+            <span className="lg:px-5 lg:py-2 md:px-3 md:py-3 sm:px-2 sm:py-2 ">
+              About
+            </span>
+            <span className="lg:px-5 lg:py-2 md:px-3 md:py-3 sm:px-2 sm:py-2 ">
+              Contact
+            </span>
+            <span className="login-btn lg:px-5 lg:py-2 md:px-3 md:py-3 sm:px-2 sm:py-2 ">
+              Login
+            </span>
             <button className="bg-red-500 text-white font-medium py-2 px-7 rounded-full">
               Sign Up
             </button>
